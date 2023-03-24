2cc8933d95e752196eecc966fdd88dece9ee33cf
Christian Medina <christianmedina121@hotmail.com>
2023-03-24 14:12:17 -0400

    Fixed typo in get certificate script. Fixed typo in app.js

--- .platform/hooks/postdeploy/00_get_certificate.sh
+++ .platform/hooks/postdeploy/00_get_certificate.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-sudo certbot -n -d http://starcaserver-env.eba-iccueqim.us-east-2.elasticbeanstalk.com/ --nginx --agree-tos --email christianmedina121@hotmail.com
+sudo certbot -n -d starcaserver-env.eba-iccueqim.us-east-2.elasticbeanstalk.com/ --nginx --agree-tos --email christianmedina121@hotmail.com
