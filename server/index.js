42b12d7ebbd7b91640920f62d490534f8260892a
jvs37 <jvs37@njit.edu>
2023-04-11 13:15:04 -0400

    update

--- server/index.js
+++ server/index.js
@@ -5,32 +5,46 @@ const bodyParser = require('body-parser');
 const bcrypt = require("bcrypt");
 const session = require('express-session');
 const nodemailer = require('nodemailer');
+const multer = require('multer');
+const path = require("path");
+const dotenv = require("dotenv");
+const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
+const {promisify} = require("util");
+
+dotenv.config({ path:'./.env'});
 
 //Middleware
 // parse incoming requests
+
 app.use(bodyParser.json());
+app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use(session({
+  secret: 'mysecret',
+  resave: false,
+  saveUninitialized: false
+}));
 
-// define middleware to check if the user is authenticated
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage })/*.array('images',5)*/;
 
-/*
-const authenticateUser = (req, res, next) => {
-    if (req.session.userId) {
-      next();
-    } else {
-      res.redirect('/login');
-    }
-  };
-*/
+
+app.get("/", async(req, res)=>{
+  res.send("this is landing page");
+});
+
+
+
 
 app.post('/register', async (req, res) => {
 
-    const { email, passwrd, confirmPassword, fname, lname, phnum } = req.body;
+    const { email, passwrd, confirmPassword, ufname, ulname, uphnum, ustreet, ucity, ustate, uzip } = req.body;
 
     try {
   
-        if (!email || !passwrd || !confirmPassword || !fname || !lname || !phnum) {
+        if (!email || !passwrd || !confirmPassword || !ufname || !ulname || !uphnum || !ustreet || !ucity || !ustate || !uzip) {
         return res.status(400).send('All Fields are Required.');
         }
     
@@ -50,8 +64,8 @@ app.post('/register', async (req, res) => {
         const hashedPassword = await bcrypt.hash(passwrd, salt);
 
         // Insert the user into the database
-        const insertQuery = 'INSERT INTO susers(email, passwrd, fname, lname, phnum) VALUES($1, $2, $3, $4, $5) RETURNING id';
-        const values = [email, hashedPassword, fname, lname, phnum];
+        const insertQuery = 'INSERT INTO susers(email, passwrd, ufname, ulname, uphnum,  ustreet, ucity, ustate, uzip) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING id';
+        const values = [email, hashedPassword, ufname, ulname, uphnum,  ustreet, ucity, ustate, uzip];
         const insertUserResult = await pool.query(insertQuery, values);
         
         res.status(201).send('You have successfully registered!');
@@ -76,10 +90,7 @@ app.post('/register', async (req, res) => {
           transporter.sendMail(mailOptions);
           console.log('Notification email sent to ${email}');
        
-        //const userId = insertUserResult.rows[0].id;
-        //req.session.userId = userId;
-
-        //res.redirect('/login');
+          //res.redirect('/login');
 
     }catch (error) {
         console.log(error);
@@ -102,6 +113,9 @@ app.post('/login', async (req, res) => {
         const query = 'SELECT * FROM susers WHERE email = $1';
         const result = await pool.query(query, [email]);
         const user = result.rows[0];
+        const userId = user.id;
+        req.session.userId = userId;
+
 
         // Check if a user with the given email exists
         if (!user) {
@@ -112,11 +126,29 @@ app.post('/login', async (req, res) => {
         const passwordMatch = await bcrypt.compare(passwrd, user.passwrd);
         if (!passwordMatch) {
             return res.redirect('/login?error=Incorrect email or password');
-        }
+        } else {
+          //const id = result[0].id;
+
+          const token = jwt.sign({userId}, process.env.JWT_SECRET, {
+              expiresIn: process.env.JWT_EXPIRES_IN
+          });
+
+          console.log("The token is: " + token);
+
+          const cookieOptions = {
+              expires: new Date( 
+                  Date.now() + process.env.JWT_COOKIE_EXPIRES * 24 * 60 * 60 * 1000 
+              ),
+              httpOnly:true
+          }
+
+          res.cookie('jwt', token, cookieOptions );
+         // res.status(200).redirect("/profile");
+      } 
+
 
         res.send("You Logged in.!");
-           
-        //req.session.userId = user.id;
+      
         //res.redirect('/profile');
 
     } catch (err) {
@@ -126,54 +158,119 @@ app.post('/login', async (req, res) => {
   });
 
   
-/*
-app.get('/logout', (req, res) => {
-    // Clear the user ID from the session
-    req.session.id = null;
-    
-    // Redirect the user to the login page or home whatever you decide
-    res.redirect('/login');
-  });
+exports.isLoggedIn = async(req, res, next) =>{
+  // console.log(req.cookies);
+   if(req.cookies.jwt){
+    try {
+        // 1. Verify the token
+        const decoded = await promisify(jwt.verify)(req.cookies.jwt,process.env.JWT_SECRET);
+            console.log(decoded);
 
+        // 2. User Existense Check
+        db.query("SELECT * FROM susers WHERE id = ?",[decoded.id], (error, result) =>{
+            console.log(result);
 
-app.get('/profile', (req, res) => {
-    // Check if the user is logged in
-    if (!req.session.id) {
-      // Redirect the user to the login page
-      res.redirect('/login');
-      return;
+            if(!result){
+                return next();
+            }
+
+            req.user = result[0];
+            return next();
+        })
+
+    } catch (error) {
+        console.log(error);
+        return next();
     }
-  
-    // Get the user data from the database
-    pool.query('SELECT * FROM susers WHERE id = $1', [req.session.id], (err, result) => {
-        
-        console.log(req.session.id);
+   }else {
+    next();
+   }
+}
 
-        if (err) {
-        console.error(err);
-        res.status(500).send('Not connecting to the server');
-        return;
-      }
-      
-      if (result.rows.length === 0) {
-        // No user found with the given ID
-        res.status(404).send('User not found');
-        return;
-      }
-      
-      // Render the profile page with the user data
-      const user = result.rows[0];
-      res.render('profile', { user });
+
+app.get('/logout',(req, res) => {
+   
+    res.cookie('jwt', 'logout',{
+      expires: new Date(Date.now() + 2*1000),
+      httpOnly: true
     });
+
+    res.status(200).redirect("/");
   });
-  */
 
 
-
-
+app.get('/profile',async(req, res,next) => {
+   
+  });
   
+  app.post('/listing',upload.array('images',5), async (req, res) => {
+ 
+    const{ltitle, ldescr, llen, lwid, lheight, lprice, lstreet,lcity, lstate, lzip, lcountry} = req.body;
+    const images = req.files;
+    //const userId = req.user.id;
+
+    try {
+      const insertListing = await pool.query(
+        'INSERT INTO slistings(ltitle, ldescr, llen, lwid, lheight, lprice, lstreet,lcity, lstate, lzip, lcountry) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11,) RETURNING *',
+        [ltitle, ldescr, llen, lwid, lheight, lprice, lstreet,lcity, lstate, lzip, lcountry]
+      );
+
+      if (insertListing.rows.length > 0) {
+        const lid = insertListing.rows[0].id;
+        console.log(lid);
+      } else {
+        console.error('Failed to insert listing');
+        res.status(500).json({ message: 'Failed to insert listing' });
+      }
+     // const lid = insertListing.rows[0].id;
+    //  console.log(lid);
+
+      const insertImages = images.map((image)=> {
+        
+        const imageQuery = 'INSERT INTO slistImages(listid, filename, filedata) VALUES ($1, $2, $3)';
+        const imageData = image.buffer;
+        const imageValues = [lid, image.filename, imageData];
+        
+        return {query: imageQuery , values: imageValues};
+
+      });
+
+      await Promise.all(imageImages.map((q) => pool.query(q.query, q.values)));
+
+      res.status(200).json({message: 'New Listing Created Successfully'}); 
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).json({ message: 'Server Error' });
+    }
+  });
+
+
+  app.get('/glisting', async (req, res) => {
+    const id = req.params.id;
+  
+    try {
+      const query = `
+        SELECT s.*, li.filename, li.filedata
+        FROM slistings s
+        INNER JOIN slistImages li ON s.LID = li.listid
+        WHERE s.LID = $1;
+      `;
+      const values = [id];
+  
+      const result = await pool.query(query, values);
+  
+      if (result.rowCount === 0) {
+        return res.status(404).json({ message: 'Listing not found' });
+      }
+  
+      res.status(200).json(result.rows[0]);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).json({ message: 'Server Error' });
+    }
+  });
+  
+
 app.listen(3001,()=>{
     console.log("Running on 3001");
 });
-
-
